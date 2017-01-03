bcb24dca623aed7582b6de6085369214e2cbc3cc
Arthur Rump <arthurrump@outlook.com>
2017-01-03 23:57:25 +0100

    Add ability to view Announcement titles
    Update Zermelo.API to fix issues with Akavache. Zie Zermelo.API #17

--- Zermelo.App.UWP/ViewModels/AnnouncementsViewModel.cs
+++ Zermelo.App.UWP/ViewModels/AnnouncementsViewModel.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Template10.Mvvm;
+using Zermelo.API.Models;
+using Zermelo.App.UWP.Services;
 
 namespace Zermelo.App.UWP.ViewModels
 {
     public class AnnouncementsViewModel : ViewModelBase
     {
+        IZermeloService _zermelo;
+
+        public AnnouncementsViewModel(IZermeloService zermelo)
+        {
+            _zermelo = zermelo;
+
+            _zermelo.GetAnnouncements()
+                .Subscribe(a => Announcements = a);
+        }
+
+        IEnumerable<Announcement> announcements;
+        public IEnumerable<Announcement> Announcements
+        {
+            get => announcements;
+            set
+            {
+                announcements = value;
+                RaisePropertyChanged();
+            }
+        }
     }
 }
