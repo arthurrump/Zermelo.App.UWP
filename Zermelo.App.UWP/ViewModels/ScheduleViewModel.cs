43019b6b8801ebc56cb3e2d89a1a30212167c981
Arthur Rump <arthurrump@outlook.com>
2017-01-04 14:55:32 +0100

    Improve basic ScheduleView layout

--- Zermelo.App.UWP/ViewModels/ScheduleViewModel.cs
+++ Zermelo.App.UWP/ViewModels/ScheduleViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Template10.Mvvm;
 using Windows.UI.Xaml;
+using Zermelo.API.Models;
 using Zermelo.App.UWP.Services;
 
 namespace Zermelo.App.UWP.ViewModels
@@ -12,10 +13,12 @@ namespace Zermelo.App.UWP.ViewModels
     public class ScheduleViewModel : ViewModelBase
     {
         ISettingsService _settings;
+        IZermeloService _zermelo;
 
-        public ScheduleViewModel(ISettingsService settings)
+        public ScheduleViewModel(ISettingsService settings, IZermeloService zermelo)
         {
             _settings = settings;
+            _zermelo = zermelo;
 
             _settings.PropertyChanged += (sender, e) =>
             {
@@ -26,5 +29,16 @@ namespace Zermelo.App.UWP.ViewModels
 
         public Visibility GroupsVisibility => 
             _settings.ShowGroups ? Visibility.Visible : Visibility.Collapsed;
+
+        IEnumerable<Appointment> appointments;
+        public IEnumerable<Appointment> Appointments
+        {
+            get => appointments;
+            set
+            {
+                appointments = value;
+                RaisePropertyChanged();
+            }
+        }
     }
 }
