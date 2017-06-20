f3c80c0f2edf8bc2966f8ddd4d3f756c73f7222a
Arthur Rump <arthurrump@outlook.com>
2017-06-20 20:39:25 +0200

    Add diagnostic try-catch blocks to MorphInto()
    Hopefully this will give some more insights into #12

--- Zermelo.App.UWP/Helpers/ObservableCollectionExtensions.cs
+++ Zermelo.App.UWP/Helpers/ObservableCollectionExtensions.cs
@@ -14,17 +14,39 @@ namespace Zermelo.App.UWP.Helpers
             var add = second.Except(first);
             var remove = first.Except(second);
 
-            foreach (var i in remove)
-                first.Remove(i);
+            // TODO: Remove the diagnostic try-catch blocks, when #12 is fixed
+            try
+            {
+                foreach (var i in remove)
+                    first.Remove(i);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new Exception("Exception in the Remove loop.", ex);
+            }
 
-            foreach (var i in add)
-                first.Add(i);
+            try
+            {
+                foreach (var i in add)
+                    first.Add(i);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new Exception("Exception in the Add loop.", ex);
+            }
 
-            // If there are any changes to first, make sure it's in 
-            // the same order as second.
-            if (add.Count() > 0 || remove.Count() > 0)
-                for (int i = 0; i < second.Count(); i++)
-                    first.Move(first.IndexOf(second.ElementAt(i)), i);
+            try
+            {
+                // If there are any changes to first, make sure it's in 
+                // the same order as second.
+                if (add.Count() > 0 || remove.Count() > 0)
+                    for (int i = 0; i < second.Count(); i++)
+                        first.Move(first.IndexOf(second.ElementAt(i)), i);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new Exception("Exception in the reordering loop.", ex);
+            }
         }
     }
 }
