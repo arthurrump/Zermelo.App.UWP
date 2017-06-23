358111632eff2dfe340bb31cec460a5f26ab5c60
Arthur Rump <arthurrump@outlook.com>
2017-06-23 11:31:26 +0200

    Remove diagnostic code from MorphInto(); close #12

--- Zermelo.App.UWP/Helpers/ObservableCollectionExtensions.cs
+++ Zermelo.App.UWP/Helpers/ObservableCollectionExtensions.cs
@@ -9,44 +9,23 @@ namespace Zermelo.App.UWP.Helpers
 {
     static class ObservableCollectionExtensions
     {
-        public static void MorphInto<TSource>(this ObservableCollection<TSource> first, IEnumerable<TSource> second) 
+        public static void MorphInto<TSource>(this ObservableCollection<TSource> first, IEnumerable<TSource> second)
         {
             var add = second.Except(first).ToList();
             var remove = first.Except(second).ToList();
 
-            // TODO: Remove the diagnostic try-catch blocks, when #12 is fixed
-            try
-            {
-                foreach (var i in remove)
-                    first.Remove(i);
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw new Exception("Exception in the Remove loop.", ex);
-            }
 
-            try
-            {
-                foreach (var i in add)
-                    first.Add(i);
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw new Exception("Exception in the Add loop.", ex);
-            }
+            foreach (var i in remove)
+                first.Remove(i);
 
-            try
-            {
-                // If there are any changes to first, make sure it's in 
-                // the same order as second.
-                if (add.Count() > 0 || remove.Count() > 0)
-                    for (int i = 0; i < second.Count(); i++)
-                        first.Move(first.IndexOf(second.ElementAt(i)), i);
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw new Exception("Exception in the reordering loop.", ex);
-            }
+            foreach (var i in add)
+                first.Add(i);
+
+            // If there are any changes to first, make sure it's in 
+            // the same order as second.
+            if (add.Count() > 0 || remove.Count() > 0)
+                for (int i = 0; i < second.Count(); i++)
+                    first.Move(first.IndexOf(second.ElementAt(i)), i);
         }
     }
 }
