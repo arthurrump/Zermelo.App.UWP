arthurrump/Zermelo.App.UWP

commit a42ebc3f19cf71bf63dd2713ad3f0b7692a58cf2

Implement viewing schedule of groups (#6)

Zermelo.App.UWP/OtherSchedules/OtherSchedulesViewModel.cs
@@ -25,7 +25,7 @@ namespace Zermelo.App.UWP.OtherSchedules
         IZermeloService _zermelo;
         IEnumerable<SearchItem> _students = new List<SearchItem>();
         IEnumerable<SearchItem> _employees = new List<SearchItem>();
-        
+        IEnumerable<SearchItem> _groups = new List<SearchItem>();
 
         public OtherSchedulesViewModel(IZermeloService zermelo)
         {
@@ -49,6 +49,14 @@ namespace Zermelo.App.UWP.OtherSchedules
                     ex => ExceptionHelper.HandleException(ex, $"{nameof(OtherSchedulesViewModel)}.GetEmployees",
                         x => new MessageDialog(x, "Error").ShowAsync())
                 );
+
+            _zermelo.GetAllGroupsAsSearchItems()
+                .ObserveOnDispatcher()
+                .Subscribe(
+                    e => _employees = e,
+                    ex => ExceptionHelper.HandleException(ex, $"{nameof(OtherSchedulesViewModel)}.GetGroups",
+                        x => new MessageDialog(x, "Error").ShowAsync())
+                );
         }
 
         private void SearchDelegate()
@@ -56,6 +64,7 @@ namespace Zermelo.App.UWP.OtherSchedules
             SearchItems.MorphInto(
                 _students
                     .Concat(_employees)
+                    .Concat(_groups)
                     .Where(s => s.DisplayText.ToLowerInvariant().Contains(SearchText.ToLowerInvariant()))
                     .OrderBy(s => s.Type)
                     .ThenBy(s => s.Code)
@@ -78,7 +87,10 @@ namespace Zermelo.App.UWP.OtherSchedules
                     text = $"Rooster van {employee.FullName}";
                     break;
                 case ScheduleType.Group:
-                    throw new NotImplementedException();
+                    var group = await _zermelo.GetGroup(SelectedSearchItem.Code);
+                    symbolText = group.Name.Substring(0, 2).ToUpperInvariant();
+                    text = $"Rooster van {group.ExtendedName}";
+                    break;
                 case ScheduleType.Location:
                     throw new NotImplementedException();
                 default:

Zermelo.App.UWP/OtherSchedules/SearchItem.cs
@@ -18,6 +18,13 @@ namespace Zermelo.App.UWP.OtherSchedules
             DisplayText = $"{user.FullName} ({user.Code})";
         }
 
+        public SearchItem(API.Models.Group group)
+        {
+            Type = ScheduleType.Group;
+            Code = group.Id.Value.ToString();
+            DisplayText = group.ExtendedName;
+        }
+
         public ScheduleType Type { get; set; }
         public string Code { get; set; }
         public string DisplayText { get; set; }

Zermelo.App.UWP/Schedule/ScheduleViewModel.cs
@@ -103,7 +103,9 @@ namespace Zermelo.App.UWP.Schedule
                         Header = $"Rooster van {employee.FullName}";
                         break;
                     case ScheduleType.Group:
-                        throw new NotImplementedException();
+                        var group = await _zermelo.GetGroup(_code);
+                        Header = $"Rooster van {group.ExtendedName}";
+                        break;
                     case ScheduleType.Location:
                         throw new NotImplementedException();
                 }
@@ -156,7 +158,8 @@ namespace Zermelo.App.UWP.Schedule
                     observable = _zermelo.GetSchedule(Date.Date, Date.Date.AddDays(1), _code);
                     break;
                 case ScheduleType.Group:
-                    throw new NotImplementedException();
+                    observable = _zermelo.GetScheduleForGroup(Date.Date, Date.Date.AddDays(1), _code);
+                    break;
                 case ScheduleType.Location:
                     throw new NotImplementedException();
                 default:

Zermelo.App.UWP/Services/CachedZermeloService.cs
@@ -33,6 +33,19 @@ namespace Zermelo.App.UWP.Services
                 end.AddDays(7)
                );
 
+        public IObservable<IEnumerable<Appointment>> GetScheduleForGroup(DateTimeOffset start, DateTimeOffset end, string code)
+            => _cache.GetAndFetchLatest(
+                $"{nameof(GetScheduleForGroup)}({start.UtcTicks},{end.UtcTicks},{code})",
+                () => {
+                    if (_internet.IsConnected())
+                        return _zermelo.GetScheduleForGroup(start, end, code);
+                    else
+                        return Observable.Return(new List<Appointment>());
+                },
+                date => _internet.IsConnected(),
+                end.AddDays(7)
+               );
+
         public IObservable<IEnumerable<Announcement>> GetAnnouncements()
             => _cache.GetAndFetchLatest(
                 nameof(GetAnnouncements),
@@ -81,6 +94,18 @@ namespace Zermelo.App.UWP.Services
                 date => _internet.IsConnected() && DateTimeOffset.UtcNow.Subtract(date) > TimeSpan.FromDays(14)
                );
 
+        public IObservable<API.Models.Group> GetGroup(string code)
+            => _cache.GetAndFetchLatest(
+                $"{nameof(GetGroup)}({code})",
+                () => {
+                    if (_internet.IsConnected())
+                        return _zermelo.GetGroup(code);
+                    else
+                        return Observable.Return(default(API.Models.Group));
+                },
+                date => _internet.IsConnected() && DateTimeOffset.UtcNow.Subtract(date) > TimeSpan.FromDays(14)
+               );
+
         public IObservable<IEnumerable<SearchItem>> GetAllStudentsAsSearchItems()
             => _cache.GetAndFetchLatest(
                 nameof(GetAllStudentsAsSearchItems),
@@ -99,6 +124,18 @@ namespace Zermelo.App.UWP.Services
                 () => {
                     if (_internet.IsConnected())
                         return _zermelo.GetAllEmployeesAsSearchItems();
+                    else
+                        return Observable.Return(new List<SearchItem>());
+                },
+                date => _internet.IsConnected() && DateTimeOffset.UtcNow.Subtract(date) > TimeSpan.FromDays(7)
+               );
+
+        public IObservable<IEnumerable<SearchItem>> GetAllGroupsAsSearchItems()
+            => _cache.GetAndFetchLatest(
+                nameof(GetAllGroupsAsSearchItems),
+                () => {
+                    if (_internet.IsConnected())
+                        return _zermelo.GetAllGroupsAsSearchItems();
                     else
                         return Observable.Return(new List<SearchItem>());
                 },

Zermelo.App.UWP/Services/IZermeloService.cs
@@ -9,11 +9,14 @@ namespace Zermelo.App.UWP.Services
     public interface IZermeloService
     {
         IObservable<IEnumerable<Appointment>> GetSchedule(DateTimeOffset start, DateTimeOffset end, string user = "~me");
+        IObservable<IEnumerable<Appointment>> GetScheduleForGroup(DateTimeOffset start, DateTimeOffset end, string code);
         IObservable<IEnumerable<Announcement>> GetAnnouncements();
         IObservable<API.Models.User> GetCurrentUser();
         IObservable<API.Models.User> GetStudent(string code);
         IObservable<API.Models.User> GetEmployee(string code);
+        IObservable<API.Models.Group> GetGroup(string code);
         IObservable<IEnumerable<SearchItem>> GetAllStudentsAsSearchItems();
         IObservable<IEnumerable<SearchItem>> GetAllEmployeesAsSearchItems();
+        IObservable<IEnumerable<SearchItem>> GetAllGroupsAsSearchItems();
     }
 }

Zermelo.App.UWP/Services/ZermeloService.cs
@@ -33,6 +33,20 @@ namespace Zermelo.App.UWP.Services
                         .Select(a => new Appointment(a))
                );
 
+        public IObservable<IEnumerable<Appointment>> GetScheduleForGroup(DateTimeOffset start, DateTimeOffset end, string code)
+            => Observable.FromAsync(
+                async () =>
+                    (await connection.Appointments.GetByCustomUrlOptionsAsync(new Dictionary<string, string>
+                        {
+                            { "start", start.ToUnixTimeSeconds().ToString() },
+                            { "end", end.ToUnixTimeSeconds().ToString() },
+                            { "containsStudentsFromGroupInDepartment", code },
+                            { "valid", "true" }
+                        },
+                        fields: Appointment.Fields))
+                        .Select(a => new Appointment(a))
+               );
+
         public IObservable<IEnumerable<Announcement>> GetAnnouncements()
             => Observable.FromAsync(
                 async () =>
@@ -55,6 +69,11 @@ namespace Zermelo.App.UWP.Services
                 () => connection.Users.GetByCodeAsync(code, new List<string> { "prefix", "lastName" })
                );
 
+        public IObservable<API.Models.Group> GetGroup(string code)
+            => Observable.FromAsync(
+                () => connection.Groups.GetSingleById(long.Parse(code), new List<string> { "id", "name", "extendedName" })
+               );
+
         public IObservable<IEnumerable<SearchItem>> GetAllStudentsAsSearchItems()
             => Observable.FromAsync(
                 async () => 
@@ -67,6 +86,13 @@ namespace Zermelo.App.UWP.Services
                 async () => 
                     (await connection.Users.GetEmployeesAsync(fields: new List<string> { "prefix", "lastName", "code" }))
                         .Select(e => new SearchItem(e, ScheduleType.Employee))
+               );
+
+        public IObservable<IEnumerable<SearchItem>> GetAllGroupsAsSearchItems()
+            => Observable.FromAsync(
+                async () =>
+                    (await connection.Groups.GetAllAsync(new List<string> { "id", "name", "extendedName" }))
+                        .Select(g => new SearchItem(g))
                );
     }
 }

commit 5a9bbdd94a9e0e60a5a4aac701239623409986b2

🤦 Fix stupid bug introduced in a42ebc3f
Employees weren't shown anymore... Never copy-paste

Zermelo.App.UWP/OtherSchedules/OtherSchedulesViewModel.cs
@@ -53,7 +53,7 @@ namespace Zermelo.App.UWP.OtherSchedules
             _zermelo.GetAllGroupsAsSearchItems()
                 .ObserveOnDispatcher()
                 .Subscribe(
-                    e => _employees = e,
+                    g => _groups = g,
                     ex => ExceptionHelper.HandleException(ex, $"{nameof(OtherSchedulesViewModel)}.GetGroups",
                         x => new MessageDialog(x, "Error").ShowAsync())
                 );